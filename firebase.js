8d6b1454b8eea61f54ea3b5b575fb2ae1c767661
BakaneN <mizbkn@gmail.com>
2025-03-29 04:58:50 +0200

    Switch to env var for Firebase Admin key

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
